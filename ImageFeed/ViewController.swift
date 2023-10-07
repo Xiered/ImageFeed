698af3aab197385c48e9a05e8419c8f41dfcdf37
Xiered <134545303+Xiered@users.noreply.github.com>
2023-10-07 07:52:12 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Герасимов on 07.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
