1dc79f966f872b3500d34a6cdb54b8e73100dab5
Xiered <vnyantost@gmail.com>
2023-10-15 21:21:15 +0300

    Adding dataSource, delegate outlet for tableView
    Adding 2 extensions for class

--- ImageFeed/ImageListViewController.swift
+++ ImageFeed/ImageListViewController.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+extension ImageListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension ImageListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+}
+
 class ImageListViewController: UIViewController {
 
     @IBOutlet private var tableView: UITableView!
