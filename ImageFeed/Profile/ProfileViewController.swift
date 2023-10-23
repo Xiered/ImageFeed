ff2d9473bb5c068f0cb61fa343a0c074fbd5f5ae
Xiered <vnyantost@gmail.com>
2023-10-23 23:12:14 +0300

    Added new group - profile and same named class
    Added UIImage, UIButton, UILabel (x3) in Main.Storyboard for new Stroyboard file - Profile View
    
    Added @IBOutlet for UIButton, UILabel (x3), UIImage in ProfileViewController class

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Герасимов on 23.10.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var profileImage: UIImageView!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+}
