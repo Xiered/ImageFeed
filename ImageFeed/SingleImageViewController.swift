2e502e1112d7b77ae73cc1e3a921aba963ad52c3
Xiered <vnyantost@gmail.com>
2023-10-30 09:24:09 +0300

    Added new Swift-file and class with same name: SingleImageViewController
    Added SingleImageViewController to Main.storyboard;

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Герасимов on 30.10.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
