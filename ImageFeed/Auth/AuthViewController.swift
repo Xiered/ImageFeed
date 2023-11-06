9dd3837f932da8bde02ab2185fa7634c3af6b0c5
Xiered <vnyantost@gmail.com>
2023-11-06 18:57:21 +0300

    Added new Auth View Controller in Main.storyboard and the same named class created
    Added layout for button and image in Auth View Controller in Main.storyboard

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Герасимов on 06.11.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
