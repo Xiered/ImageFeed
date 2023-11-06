6281c6d4c44e48af937e9ae3031a1d667af8932b
Xiered <vnyantost@gmail.com>
2023-11-06 20:31:53 +0300

    Added WebViewViewController in Main.storyboard and the same named class; added button (back) in class and layout;
    Added segue from Auth View Controller to WebView View Controller and identifier ShowWebView

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Герасимов on 06.11.2023.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        
+    }
+}
+
