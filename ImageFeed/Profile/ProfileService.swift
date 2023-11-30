61892c823b14f6a1d744b6a359d034fa6b4c82e1
Xiered <vnyantost@gmail.com>
2023-11-30 17:47:32 +0300

    Added variables in struct Added some in method

--- ImageFeed/Profile/ProfileService.swift
+++ ImageFeed/Profile/ProfileService.swift
@@ -2,10 +2,13 @@ import UIKit
 
 final class ProfileService {
     struct ProfileResult: Codable { // Structure for Unsplash answer decoding
-        
+        var userName: String
+        var firstName: String
+        var lastName: String
+        var bio: String
     }
     
-    struct Profile {
+    struct Profile: Codable {
         var username: String
         var name: String
         var loginName: String
@@ -14,6 +17,8 @@ final class ProfileService {
     
     func fetchProfile(token: String, completion: @escaping (Result<Profile, Error>) -> Void) {
         assert(Thread.isMainThread)
-        
+        guard let url = URL(string: "https://api.unsplash.com/me") else { return }
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
     }
 }
