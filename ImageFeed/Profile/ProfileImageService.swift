1188ffd507466a61752a60b9e35fe546b61415e6
Xiered <vnyantost@gmail.com>
2023-12-06 17:46:25 +0300

    Added ProfileImageService for request avatar image

--- ImageFeed/Profile/ProfileImageService.swift
+++ ImageFeed/Profile/ProfileImageService.swift
@@ -3,9 +3,19 @@ import UIKit
 final class ProfileImageService {
     static let shared = ProfileImageService()
     private (set) var avatarURL: String?
+    private let tokenStorage = OAuth2TokenStorage()
     
-    func fetchProfileImage(username: String, _ completion: @escaping (Result<String, Error>) -> Void) {
+    func fetchProfileImageURL(username: String, _ completion: @escaping (Result<String, Error>) -> Void) {
+        guard let token = tokenStorage.token else { return }
         
+        let urlString = "https://api.unsplash.com/users/\(username)"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        // dataTask
     }
     
 }
