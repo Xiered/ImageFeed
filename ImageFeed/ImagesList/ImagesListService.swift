2994ac7119c2a0878c2f20c2ced4b7d33187c2cf
Xiered <vnyantost@gmail.com>
2023-12-19 16:15:50 +0300

    Added models for JSON

--- ImageFeed/ImagesList/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -3,21 +3,42 @@ import UIKit
 final class ImagesListService {
     private var lastLoadedPage: Int?
     private (set) var photos: [Photo] = []
+    static let DidChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
     
     func fetchPhotosNextPage() {
         
     }
     
 }
+// MARK: - Model
 
-extension ImagesListService { // Structure for JSON-fields
-    struct Photo {
-        let id: String
-        let size: CGSize
-        let createdAt: Date?
-        let welcomeDescription: String?
-        let thumbImageURL: String
-        let largeImageURL: String
-        let isLiked: Bool
-    }
+// Structures for JSON-fields
+struct Photo {
+    let id: String
+    let size: CGSize
+    let createdAt: Date?
+    let welcomeDescription: String?
+    let thumbImageURL: String
+    let largeImageURL: String
+    let isLiked: Bool
+}
+
+struct PhotoResult: Codable {
+    let id: String
+    let createdAt: Int
+    let updatedAt: Int
+    let width: Int
+    let height: Int
+    let description: String
+    let urls: UrlsResult
+    let likes: Int
+    let likedByUser: Bool
+}
+
+struct UrlsResult: Codable {
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
 }
