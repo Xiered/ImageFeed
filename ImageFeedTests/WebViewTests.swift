0cbdc56f065ea7873417a1406797c08fc332f4cf
Xiered <vnyantost@gmail.com>
2023-12-27 02:01:15 +0300

    Added test for WebView (viewDidLoad calls method)

---
+++ ImageFeedTests/WebViewTests.swift
@@ -0,0 +1,28 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Дмитрий Герасимов on 27.12.2023.
+//
+
+import XCTest
+@testable import ImageFeed
+
+final class WebViewTests: XCTestCase{
+    
+    func testViewControllerCallsViewDidLoad() {
+        // Given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        // When
+        _ = viewController.view
+        
+        // Then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+}
+
