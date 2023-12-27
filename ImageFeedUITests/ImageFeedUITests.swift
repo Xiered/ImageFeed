b950151f6aa7a0c3ae64352f76f3a76c7254e2d5
Xiered <vnyantost@gmail.com>
2023-12-27 03:33:11 +0300

    Preparation for UI-tests (auth, feed, profile)

---
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -0,0 +1,30 @@
+//
+//  ImageFeedUITests.swift
+//  ImageFeedUITests
+//
+//  Created by Дмитрий Герасимов on 27.12.2023.
+//
+
+import XCTest
+
+final class ImageFeedUITests: XCTestCase {
+    private let app = XCUIApplication()
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        
+        app.launch()
+    }
+    
+    func testAuth() throws {
+        
+    }
+    
+    func testFeed() throws {
+        
+    }
+    
+    func testProfile() throws {
+        
+    }
+}
