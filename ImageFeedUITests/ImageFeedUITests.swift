9005f09be49a4a6acf9786d240757fc366c90e4a
Xiered <vnyantost@gmail.com>
2023-12-27 04:32:14 +0300

    Added two tests UI

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -9,6 +9,8 @@ import XCTest
 
 final class ImageFeedUITests: XCTestCase {
     private let app = XCUIApplication()
+    let email = ""
+    let password = ""
     
     override func setUpWithError() throws {
         continueAfterFailure = false
@@ -17,11 +19,60 @@ final class ImageFeedUITests: XCTestCase {
     }
     
     func testAuth() throws {
+        app.buttons["Authenticate"].tap()
         
+        let webView = app.webViews["UnsplashWebView"]
+        XCTAssertTrue(webView.waitForExistence(timeout: 5))
+        
+        let loginTextField = webView.descendants(matching: .textField).element
+        XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
+        
+        loginTextField.tap()
+        loginTextField.typeText(email)
+        app.toolbars.buttons["Done"].tap()
+        
+        let passwordTextField = webView.descendants(matching: .secureTextField).element
+        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
+        
+        passwordTextField.tap()
+        passwordTextField.typeText(password)
+        webView.swipeUp()
+        
+        print(app.debugDescription)
+        
+        webView.buttons["Login"].tap()
+        
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
     }
     
     func testFeed() throws {
+        let tablesQuery = app.tables
+        sleep(3)
         
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        cell.swipeUp()
+        
+        sleep(2)
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        cellToLike.buttons["like button"].tap()
+        sleep(2)
+        cellToLike.buttons["like button"].tap()
+        
+        sleep(2)
+        cellToLike.tap()
+        sleep(2)
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        
+        image.pinch(withScale: 3, velocity: 1)
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navBackButtonWhiteButton = app.buttons["nav_back_button"]
+        navBackButtonWhiteButton.tap()
     }
     
     func testProfile() throws {
